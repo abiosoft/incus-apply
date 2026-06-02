ec6eb53be8659f627cb9ea2be899a5b92a8213fa
Erwan d'Orgeville <info@erwandorgeville.com>
2026-06-02 12:14:43 -0400

    dev: add version, commit and date to flake for --version to work
    Signed-off-by: Erwan d'Orgeville <info@erwandorgeville.com>

--- flake.nix
+++ flake.nix
@@ -23,6 +23,12 @@
           src = ./.;
           vendorHash = "sha256-u+nl3P7YNl+3DJIXo7pnDKF4PkoYLaHf3B1LqF9b+V8=";
 
+          ldflags = [
+            "-X main.version=${self.shortRev or self.dirtyShortRev or "dev"}"
+            "-X main.commit=${self.rev or self.dirtyRev or "none"}"
+            "-X main.date=${self.lastModifiedDate or "unknown"}"
+          ];
+
           meta = {
             description = "Declarative configuration management for Incus";
             homepage = "https://github.com/abiosoft/incus-apply";
