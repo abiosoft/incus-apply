45b7cb33b153cf79d7b1cfac4869fd6cb80f25a0
Erwan d'Orgeville <info@erwandorgeville.com>
2026-06-02 11:27:37 -0400

    dev: add nix flake
    Signed-off-by: Erwan d'Orgeville <info@erwandorgeville.com>

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Declarative configuration management for Incus";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.default = pkgs.buildGo126Module {
+          pname = "incus-apply";
+          name = "incus-apply";
+          src = ./.;
+          vendorHash = "sha256-u+nl3P7YNl+3DJIXo7pnDKF4PkoYLaHf3B1LqF9b+V8=";
+        };
+      }
+    );
+}
